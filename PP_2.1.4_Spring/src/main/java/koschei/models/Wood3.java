4b6b287fdbaf3d5eb317f3add9981354700843bc
kanaOwl <100942036+kanaOwl@users.noreply.github.com>
2022-06-10 12:18:26 +0300

    Add files via upload

---
+++ PP_2.1.4_Spring/src/main/java/koschei/models/Wood3.java
@@ -0,0 +1,16 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Wood3 {
+
+    @Autowired
+    private Rabbit4 rabbit;
+
+    @Override
+    public String toString() {
+        return ", на дереве заяц " + rabbit.toString();
+    }
+}
